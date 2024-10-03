97ad6fd7105d82f7f54f8a2edf42f36b68bd75a8
dhananjay <126044351+store-future@users.noreply.github.com>
2024-10-03 23:05:22 +0530

    add view functiona;ity
    add fetching data from server to frontend

--- django-react-ToDo/frontend/src/templates/Todos.jsx
+++ django-react-ToDo/frontend/src/templates/Todos.jsx
@@ -7,18 +7,34 @@ export default function Todos() {
   const [tasks , setTasks] = useState([]);
   const [inputValue , setInputValue] = useState('')
 
-    const addTask = async () =>{
-          try{
-            if (inputValue.trim() !== ''){
-              const response = await axios.post('http://127.0.0.1:8000/api/TodoList/add',{title:inputValue , completed:false });
-              setTasks([...tasks , response.data]);   // adding two list 
-              setInputValue('');
-            }
+  //fetching value
+  useEffect( ()=>{fetchTasks();} , []);
+
+  const fetchTasks = async () =>{
+      try{
+        const response = await axios.get('http://127.0.0.1:8000/api/TodoList');
+        setTasks(response.data)
+      }
+      catch(error){
+        console.log('error' , error);
+      }
+  }
+
+  // Add tasks
+  const addTask = async () =>{
+        try{
+          if (inputValue.trim() !== ''){
+            const response = await axios.post('http://127.0.0.1:8000/api/TodoList/add',{title:inputValue , completed:false });
+            setTasks([...tasks , response.data]);   // adding two list 
+            setInputValue('');
           }
-          catch(error){
-            console.log('error',error)
-          }
-    }
+        }
+        catch(error){
+          console.log('error',error)
+        }
+  }
+
+  
 
   return (
     <div className="container">
@@ -35,8 +51,13 @@ export default function Todos() {
           />
           <button onClick={addTask}>Add</button>
         </div>
-        <ul id="list-container"></ul>
+        <ul id="list-container">
+          { tasks.map((item) => 
+            (<li key={item.id}> {item.title} </li>) 
+          )}
+        </ul>
       </div>
     </div>
   );
 }
+
